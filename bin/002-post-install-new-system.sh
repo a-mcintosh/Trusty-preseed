603ca342a30a7f4f7f5e2d25983fdf727d7df6ba
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2017-07-08 01:42:43 -0500

    Invoke 002-post-install-new-system.sh at end of install.

--- bin/002-post-install-new-system.sh
+++ bin/002-post-install-new-system.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #-------------------------------------------------------------------------------------
 #  -- Aubrey McIntosh, Ph.D.  $Id$
-#  -- 2nd phase of virtual machine setup
-#  -- This should all be in preseed when I learn how.
+#  -- Some configuration for the new virtual machine, invoked at the end of 
+#  -- preseed/iounote.seed
 #  --
 #  -- rcp ubuntu@2001:470:b8ac:0:44c4:9dea:d631:db1d:~/CD-addons/1492966260/shop-jigs/projects/post-install-new-system.sh
 #-------------------------------------------------------------------------------------
@@ -10,27 +10,25 @@
 
 
 #-------------------------------------------------------------------------------------
-sudo chown -R `whoami`:`whoami` ~/.
-mkdir -p ~/bin; cd ~/bin; chmod u+w,o+wt ~/bin
+sudo chown -R 1000:1000 ~aubrey/.
 #-------------------------------------------------------------------------------------
 
-echo "export EPOCH=\`date '+%s'\`" >> ~/.profile
-echo mv .bash_history .bash_history-'$EPOCH' >> ~/.profile
-echo touch .bash_history >> ~/.profile
+echo "export EPOCH=\`date '+%s'\`" >> ~aubrey/.profile
+echo mv .bash_history .bash_history-'$EPOCH' >> ~aubrey/.profile
+echo "touch .bash_history" >> ~aubrey/.profile
 
 #-------------------------------------------------------------------------------------
 sudo sed -i "s/ubuntu/iounote/g" /etc/hostname
 sudo sed -i "s/ubuntu/iounote/g" /etc/hosts
 
 #-------------------------------------------------------------------------------------
-#  -- set up ability to get updates from local apt-ng server, 
+#  -- the apt proxy is already set-up during the install, /etc/apt/apt.conf, 
 #  -- get openssh-server; load backdoor pass (authorized_keys) from web server
 #  -- close off password login
 #-------------------------------------------------------------------------------------
 
-echo 'Acquire::http { Proxy "http://lifepod13.quarantine.vima.austin.tx.us:3142"; };' > 01proxy
-sudo mv 01proxy /etc/apt/apt.conf.d/.
-sudo apt-get install openssh-server
+sudo apt-get update
+sudo apt-get install openssh-server ntp git
 sudo sed -i 's|[#]*PasswordAuthentication yes|PasswordAuthentication no|g' /etc/ssh/sshd_config
 sudo service ssh restart
 
@@ -40,12 +38,11 @@ sudo service ssh restart
 
 sudo sed -i "/^exit 0$/iip addr add 2001:470:b8ac::2017:402/64 dev eth0" /etc/rc.local
 sudo sed -i "/^exit 0$/iip addr add 2001:470:b8ac::1:6/64 dev eth0" /etc/rc.local
-ssh-keygen -N "This is not the real pass-phrase." -f ~/.ssh/id_rsa
-(cd ~/.ssh
+ssh-keygen -N "This is not the real pass-phrase." -f ~aubrey/.ssh/id_rsa
+(cd ~aubrey/.ssh
   wget http://lifepod13/ssh-pubkeys/authorized_keys
 )
 
 
 #-------------------------------------------------------------------------------------
-sudo apt-get install ntp git
 
