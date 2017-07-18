68f935c76bb78380aaac04ff5f8b5dbc97f38b30
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2017-07-17 22:04:12 -0500

    successful file install, need permissions & security.

--- bin/003-early-command.sh
+++ bin/003-early-command.sh
@@ -3,7 +3,15 @@
 #  -- Aubrey McIntosh, Ph.D.  $Id$
 #-------------------------------------------------------------------------------------
 cp -pr /cdrom/bin/ /tmp/
-chmod -R go=,u=rwx /tmp/bin/.ssh
-chmod        u=rw  /tmp/bin/.ssh
+chmod -R go=,u=rw  /tmp/bin/.ssh
+chmod        u=rwx /tmp/bin/.ssh
 ls -laR /tmp/bin
+
+sed -i s/passwd.username/$(debconf-get passwd/username)/g /tmp/bin/*.sh
+logger -i "$(debconf-get passwd/username)"
+logger -i "$(debconf-get netcfg/hostname)"
+logger -i "$(debconf-get netcfg/get_domain)"
+logger -i "$(debconf-get netcfg/choose_interface)"
+#ls -laR /usr/lib/ubiquity
+export DISPLAY=:0.0
 
