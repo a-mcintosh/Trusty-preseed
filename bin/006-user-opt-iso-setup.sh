1981ef98d5c399491fd2680007a6c924874bf6d1
Aubrey McIntosh, PhD <aubrey.mcintosh@utexas.edu>
2017-07-18 09:57:20 -0500

    Successful build, restores git & web servers.

--- bin/006-user-opt-iso-setup.sh
+++ bin/006-user-opt-iso-setup.sh
@@ -4,14 +4,9 @@
 #  -- setup the opt directory
 #-------------------------------------------------------------------------------------
 umask 0067
-mkdir -p /target/home/passwd.username/opt/
-
 
 #-------------------------------------------------------------------------------------
-
-mkdir -p /target/mnt/passwd.username/opt
-mount /dev/sr3 /target/mnt/passwd.username/opt
-cp -pr /target/mnt/passwd.username/opt/ /target/home/passwd.username/
-chown -R 1000 /target/home/passwd.username/opt
+cp -pr /mnt/username/opt/ /home/passwd.username/
+chown -R passwd.username:passwd.username ~passwd.username
 xterm -title opt
 
